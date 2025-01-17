3cd053cc0e2e1e2630025a2a943b1855403acf81
Marcos Vinicio <141582289+marcosvinicio10@users.noreply.github.com>
2025-01-17 15:38:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ const eventsSwiper = new Swiper('.eventsSwiper', {
     },
 });
 
+const hamburger = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('.nav');
+
+// Toggle menu ao clicar no botão hambúrguer
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('visible');
+});
+
+// Fechar menu ao clicar em um link
+document.querySelectorAll('.nav ul li a').forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('visible');
+    });
+});
+
